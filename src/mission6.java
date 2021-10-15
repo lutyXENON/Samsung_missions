c756fb73be3579e92669655dc2cb70d351383f2c
lutyXENON <81015157+lutyXENON@users.noreply.github.com>
2021-10-15 19:25:51 +0300

    Mission 6 is completed!

---
+++ src/mission6.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class mission6 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int max11 = 0;
+        for (int i = 0; i < n; i++) {
+            int input = in.nextInt();
+            if (input % 100 == 11 && input > max11)
+                max11 = input;
+        }
+        System.out.println(max11);
+    }
+}
