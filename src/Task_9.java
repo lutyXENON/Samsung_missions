4c133b11065224449665642cce85dffbdb73b8d3
lutyXENON <81015157+lutyXENON@users.noreply.github.com>
2021-10-17 16:07:48 +0300

    Сделал 9 миссию, но с помощью строк.

---
+++ src/Task_9.java
@@ -0,0 +1,14 @@
+import java.util.Objects;
+import java.util.Scanner;
+
+public class Task_9 {
+    public static String reverseString(String str) {
+        return new StringBuilder(str).reverse().toString();
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String n = in.nextLine();
+        System.out.println(Objects.equals(n, reverseString(n)) ? "Yes" : "No");
+    }
+}
