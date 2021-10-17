3ceb79e14e914437fb1fa170d6b73f0279b38d75
lutyXENON <81015157+lutyXENON@users.noreply.github.com>
2021-10-17 15:50:56 +0300

    Сделал 8 миссию, но осталась звездочка на конце.

---
+++ src/Task_8.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+import static java.lang.Math.sqrt;
+
+public class Task_8 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int b = n;
+        for (int i = 2; i < sqrt(b); i++) {
+            if (n % i == 0) {
+                System.out.print(i + "*");
+                n /= i;
+            }
+        }
+    }
+}
